fb989b1a928a24ea32f61767d5ea92bdb36537a2
arrudafdc <arrudafdc@gmail.com>
2023-02-27 12:24:24 -0300

    corrige o bug scroll do header

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const btnMobile = document.querySelector('.btn-mobile');
+const body = document.querySelector('body')
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.querySelector('nav');
   nav.classList.toggle('active');
+  body.classList.toggle('no-scroll')
   
   const active = nav.classList.contains('active')
   event.currentTarget.setAttribute('aria-expanded', active)
